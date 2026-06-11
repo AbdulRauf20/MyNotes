becad48126abbf2dea09a93831f8f34823e081fd
AbdulRauf <mrrauf228@gmail.com>
2026-06-12 00:24:33 +0500

    notes route added

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -62,6 +62,9 @@ class _LoginViewState extends State<LoginView> {
                       email: email,
                       password: password,
                     );
+                Navigator.of(
+                  context,
+                ).pushNamedAndRemoveUntil('/notes/', (route) => false);
                 final user = userCredential.user;
 
                 if (user != null && !user.emailVerified) {
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -66,14 +67,14 @@ class _RegisterViewState extends State<RegisterView> {
                   ),
                 );
 
-                print(userCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == "Weak-password") {
-                  print("Weak-password");
+                  devtools.log("Weak-password");
                 } else if (e.code == "email-already-in-use") {
-                  print("email-already-in-use");
+                  devtools.log("email-already-in-use");
                 } else if (e.code == "invalid-email") {
-                  print("invalid-email");
+                  devtools.log("invalid-email");
                 }
               }
             },
